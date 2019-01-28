e4831049503efb910ac239bb4aeaa9e58b633089
Bidkar Aragón Cárdenas <bidkar@gmail.com>
2019-01-28 11:56:07 -0700

    JS
    formas de incluir Js en HTML
    funcionalidad de JS para el boton submit

--- 02-login-form/js/app.js
+++ 02-login-form/js/app.js
@@ -1,1 +1,13 @@
 // Funcionalidad
+function validateLogin(e) {
+    // TODO: detener la accion de submit
+    e.preventDefault();
+    console.log("he pasado por aqui");
+    // TODO: recoger los valores de las cajas de entrada
+    let usuario = document.getElementById("txtUsuario").value;
+    // let usuario = document.querySelector("#txtUsuario");
+    let contraseña = document.getElementById("txtContrasena").value;
+    console.log(`usuario ${usuario} contraseña ${contraseña}`);
+}
+
+document.querySelector("#login-form>form>button[type=submit]").addEventListener("click", validateLogin);
