d612b844f934b3d7690828d293406a77d714efaf
Bidkar Aragón Cárdenas <bidkar@gmail.com>
2019-01-29 11:48:40 -0700

    ASP.Net
    Mi primer proyecto MVC

---
+++ 03-primer-proyecto-asp.net-core/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace _03_primer_proyecto_asp.net_core.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
