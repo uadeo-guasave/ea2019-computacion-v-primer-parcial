9494961cc42f19ad14a49ff88e09b4a87bc98a12
Bidkar Aragón Cárdenas <bidkar@gmail.com>
2019-01-31 12:00:38 -0700

    OOP C#
    + se utilizó el CLI de Net Core
    + se agregó proyecto de consola
    + se dió inicio a la temática de POO

---
+++ 04-OOP-Console-App/Alumno.cs
@@ -0,0 +1,13 @@
+namespace _04_OOP_Console_App
+{
+    public class Alumno
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        
+        public Alumno()
+        {
+            
+        }
+    }
+}
