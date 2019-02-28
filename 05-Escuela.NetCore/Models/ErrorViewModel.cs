c865fb6eeb053ad56fc0c9b4ea94304c44595c08
Bidkar Aragón Cárdenas <bidkar@gmail.com>
2019-02-28 11:16:11 -0700

    Asp.Net MVC

---
+++ 05-Escuela.NetCore/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace _05_Escuela.NetCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
