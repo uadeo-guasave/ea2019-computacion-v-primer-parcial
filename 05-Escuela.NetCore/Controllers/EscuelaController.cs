a5c5314030043a4ebd053c521b773c52160d7b1d
Bidkar Aragón Cárdenas <bidkar@gmail.com>
2019-03-04 11:40:03 -0700

    Models and Views

--- 05-Escuela.NetCore/Controllers/EscuelaController.cs
+++ 05-Escuela.NetCore/Controllers/EscuelaController.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using _05_Escuela.NetCore.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace _05_Escuela.NetCore.Controllers
@@ -6,7 +8,28 @@ namespace _05_Escuela.NetCore.Controllers
     {
         public IActionResult Index()
         {
-            return View();
+            var escuelas = new List<Escuela>
+            {
+                new Escuela {Id=1,Nombre="UAdeO",Ubicación="Guasave, Sinaloa",Director="Fridzia Izaguirre Diaz de León"},
+                new Escuela {Id=2,Nombre="UAS",Ubicación="Culiacan, Sinaloa",Director="Dire de culichi"},
+                new Escuela {Id=3,Nombre="UNIVAFU",Ubicación="Los Mochis, Sinaloa",Director="El de mochis"}
+            };
+            return View(escuelas);
+        }
+
+        public IActionResult Detalle(int EscuelaId)
+        {
+            Escuela escuela;
+            if (EscuelaId == 1)
+            {
+                escuela = new Escuela {Id=1,Nombre="UAdeO",Ubicación="Guasave, Sinaloa",Director="Fridzia Izaguirre Diaz de León"};
+            }
+            else
+            {
+                escuela = null;
+            }
+            
+            return View(escuela);
         }
     }
 }
---
+++ 05-Escuela.NetCore/Models/Escuela.cs
@@ -0,0 +1,10 @@
+namespace _05_Escuela.NetCore.Models
+{
+    public class Escuela
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Ubicación { get; set; }
+        public string Director { get; set; }
+    }
+}
